04216b81067c0d1d0b70c66f8ba8cfe19b847351
Yono Raphael <yonoraphael@gmail.com>
2021-01-16 13:59:29 +0700

    16-1-21

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Suyono`,
+    description: `Seorang web developer serta designer dan juga seorang wibu`,
+    author: `@suyono`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
